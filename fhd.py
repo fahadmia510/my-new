dcaa104b987d973b7c4d1e5373bc5aeeecd9e0bf
fahadmia510 <fahadmia510@gmail.com>
2025-08-03 04:12:17 +0600

    Add files via upload

---
+++ fhd.py
@@ -0,0 +1,19 @@
+# Simple Calculator using print only
+
+print("Welcome to Simple Calculator")
+print("We will calculate 10 + 5")
+print("Result:", 10 + 5)
+
+print("Now we calculate 85 - 855")
+print("Result:", 85 - 855)
+
+print("Now we calculate 6 * 4")
+print("Result:", 6 * 4)
+
+print("Now we calculate 50 / 2")
+print("Result:", 50 / 2)
+
+
+print("Result","Result")
+print("Result",10+5)
+
